7bf22467596189521dc5d272f6e4f31f56766259
Pete Hodgson <git@thepete.net>
2015-03-06 14:12:31 -0800

    update version, fix typo

--- examples/upcase-http.js
+++ examples/upcase-http.js
@@ -6,6 +6,6 @@ Cuddles.monkeyPatchBacon();
 
 var upcase = function(x){ return x.toString().toUpperCase() };
 
-Bacon.fromNodeStream( request.get('https://api.spotify.com/v1/search?type=artist&q=aphex+twin') );
+Bacon.fromNodeStream( request.get('https://api.spotify.com/v1/search?type=artist&q=aphex+twin') )
   .map( upcase )
   .pipeInto( process.stdout );

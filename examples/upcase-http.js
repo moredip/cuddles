97079fc0a1a23ab3d25ef1edf80d8cbff5ec11df
Pete Hodgson <git@thepete.net>
2015-08-12 11:48:35 -0700

    enhance http example

--- examples/upcase-http.js
+++ examples/upcase-http.js
@@ -2,10 +2,26 @@ var Bacon = require('baconjs').Bacon,
     Cuddles = require('../index.js')(Bacon);
     request = require('request');
 
+var TAYLOR_SWIFT = "06HL4z0CvFAxyc27GXpf02";
+
+var urlForArtistsRelatedTo = function(artistId){
+  return "https://api.spotify.com/v1/artists/"+artistId+"/related-artists";
+}
+
+var pluckArtistNamesFromResponse = function(jsonResponse){
+  return jsonResponse.artists.map( function(artistJson){
+    return artistJson.name;
+  });
+}
+
 Cuddles.monkeyPatchBacon();
 
 var upcase = function(x){ return x.toString().toUpperCase() };
 
-Bacon.fromNodeStream( request.get('https://api.spotify.com/v1/search?type=artist&q=aphex+twin') )
+Bacon.fromNodeStream( request.get( urlForArtistsRelatedTo(TAYLOR_SWIFT) ) )
+  .map( JSON.parse )
+  .map( pluckArtistNamesFromResponse )
+  .flatMap( Bacon.fromArray )
   .map( upcase )
+  .map( function(x){ return x+"\n"; } )
   .pipeInto( process.stdout );

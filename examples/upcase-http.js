2f4cba683ffa3dee7529e9d3c3fba3e2e66dcc44
Pete Hodgson <git@thepete.net>
2015-08-12 12:11:58 -0700

    upcase http example now served up over http

--- examples/upcase-http.js
+++ examples/upcase-http.js
@@ -1,8 +1,7 @@
 var Bacon = require('baconjs').Bacon,
-    Cuddles = require('../index.js')(Bacon);
+    Cuddles = require('../index.js')(Bacon),
+    http = require('http'),
     request = require('request');
-
-var TAYLOR_SWIFT = "06HL4z0CvFAxyc27GXpf02";
 
 var urlForArtistsRelatedTo = function(artistId){
   return "https://api.spotify.com/v1/artists/"+artistId+"/related-artists";
@@ -14,14 +13,34 @@ var pluckArtistNamesFromResponse = function(jsonResponse){
   });
 }
 
+var streamOfNamesForArtistsRelatedTo = function(artistId){
+  //return Bacon.fromNodeStream( request.get( urlForArtistsRelatedTo(artistId) ) )
+  return Bacon.fromNodeStream( require('fs').createReadStream('/Users/phodgson/tmp/related-artists.json') )
+    .map( JSON.parse )
+    .map( pluckArtistNamesFromResponse )
+    .flatMap( Bacon.fromArray );
+}
+
+var appender = function(toAppend){
+  return function(x){ return x+toAppend; };
+}
+
 Cuddles.monkeyPatchBacon();
 
-var upcase = function(x){ return x.toString().toUpperCase() };
+var server = http.createServer( function(req,res) {
+  var artistName = require('url').parse(req.url).pathname.split("/")[1];
 
-Bacon.fromNodeStream( request.get( urlForArtistsRelatedTo(TAYLOR_SWIFT) ) )
-  .map( JSON.parse )
-  .map( pluckArtistNamesFromResponse )
-  .flatMap( Bacon.fromArray )
-  .map( upcase )
-  .map( function(x){ return x+"\n"; } )
-  .pipeInto( process.stdout );
+  res.write("Artists related to ");
+  res.write(artistName);
+  res.write("\n---\n\n");
+
+  streamOfNamesForArtistsRelatedTo(artistName)
+    .map(".toUpperCase")
+    .map(appender("\n"))
+    .pipeInto(res);
+});
+
+var port = (process.argv[2] || 8000);
+console.log("SERVER LISTENING ON PORT "+port);
+
+server.listen(port);
